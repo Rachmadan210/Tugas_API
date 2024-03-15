0e1be0cbc5ca5c848a88b2200b8e36a4cce255bb
22091397072_Rachmadan Nauval <144086273+Rachmadan210@users.noreply.github.com>
2024-03-15 17:43:27 +0700

    Add files via upload

---
+++ ContactSeeder.php
@@ -0,0 +1,20 @@
+<?php
+namespace Database\Seeders;
+use Illuminate\Database\Seeder;
+use App\Models\Contact;
+
+class ContactSeeder extends Seeder
+{
+    public function run()
+    {
+        Contact::create([
+            'first_name' => 'John',
+            'last_name' => 'Doe',
+            'email' => 'john@example.com',
+            'phone' => '123456789'
+        ]);
+
+        // Tambahkan data lainnya jika diperlukan
+    }
+}
+?>
